4935db58110c0851685a36414bc06ccfbdb95732
risyasfr <risyasfr@gmail.com>
2025-07-04 15:55:18 +0000

    Suggestions in energy & stress overview

--- src/app/(main)/insights/page.tsx
+++ src/app/(main)/insights/page.tsx
@@ -84,11 +84,11 @@ export default function InsightsPage() {
                     </div>
                 </div>
                 {/* Most Frequent Stress Triggers */}
-                <div className="mb-4">
+                <div className="mb-2">
                   <h4 className="font-semibold text-[#000000] text-base">Most Frequent Stress Trigger</h4>
                 </div>
                 <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
-                  <ul className="text-sm text-[#000000] list-disc ml-4 mr-4">
+                  <ul className="text-sm text-[#000000] list-disc ml-4 mr-3">
                     <li>Deadlines</li>
                     <li>Lack of Sleep</li>
                     <li>Presentation Panic</li>
@@ -102,22 +102,29 @@ export default function InsightsPage() {
                     </div>
                   </div>
                 </div>
+                {/* Suggestions */}
+                <div className="mt-4 mb-3">
+                    <h4 className="font-semibold text-[#000000] text-base mb-2">Suggestions</h4>
+                    <p className="text-sm text-[#000000] text-justify">
+                    You’ve mentioned ‘Deadlines’ and ‘Lack of Sleep’ 4 times this week. No wonder you feel worried, overwhelmed, and tired! Try breaking your assignments into smaller chunks and reward yourself for each one you complete.
+                    </p>
+                </div>
             </div>
 
             {/* Highlights */}
             <div className ="bg-[#FCE8DB] p-6 rounded-2xl">
                 <h3 className="text-lg font-extrabold text-gray-800 mb-6 text-center md:text-left">Highlights</h3>
                 <ul className="list-disc ml-5 text-sm text-[#000000] mb-4">
-                    <li>Checked in 7 days in a row</li>
-                    <li>Slept more than 6 hours 3 times</li>
-                    <li>Successfully completed 3 micro-therapy sessions</li>
-                    <li>Noticed feelings of anxiety and labeled them appropriately</li>
-                    <li>Wrote 1 honest reflection about feeling overwhelmed</li>
+                    <li className="mb-1">Checked in 7 days in a row</li>
+                    <li className="mb-1">Slept more than 6 hours 3 times</li>
+                    <li className="mb-1">Successfully completed 3 micro-therapy sessions</li>
+                    <li className="mb-1">Noticed feelings of anxiety and labeled them appropriately</li>
+                    <li className="mb-1">Wrote 1 honest reflection about feeling overwhelmed</li>
                 </ul>
 
                 { /* Quotes */}
                 <div>
-                    <h6 className="italic font-semibold text-[#000000] text-xs text-center mb-2">“Small steps count. You’ve walked, that’s what’s important.”</h6>
+                    <h6 className="italic font-semibold text-[#000000] text-base text-center mb-2">“Small steps count. You’ve walked, that’s what’s important.”</h6>
                 </div>
             </div>
         </div>
